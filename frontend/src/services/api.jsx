a9ca086f3503bbad82edee78bd78f9c058cf1995
sumathi-4 <sumathisaraswathi4@gmail.com>
2026-05-05 11:35:47 +0530

    error fixed

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "https://saas-app-backend.onrender.com/api"
+ baseURL: "https://saas-app-backend-71pr.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
